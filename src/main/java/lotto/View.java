a94577a6697e31e130b7eba5e3d70410c62ff848
kanai1 <ksh30918@kakao.com>
2025-10-28 18:02:24 +0900

    feat(View): getLottoBonusNumber 메소드 추가
    - 보너스 번호에 관한 입출력 추가
    - 출력에 필요한 String을 상수로 관리

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -3,13 +3,23 @@ package lotto;
 import camp.nextstep.edu.missionutils.Console;
 
 public class View {
+
+    private static final String LOTTO_PURCHASE_AMOUNT_OUTPUT_STATEMENT = "구입금액을 입력해 주세요.";
+    private static final String LOTTO_WINNING_NUMBER_OUTPUT_STATEMENT = "당첨 번호를 입력해 주세요.";
+    private static final String LOTTO_BONUS_NUMBER_OUTPUT_STATEMENT = "보너스 번호를 입력해 주세요.";
+
     public String getLottoPurchaseAmount() {
-        System.out.println("구입금액을 입력해 주세요.");
+        System.out.println(LOTTO_PURCHASE_AMOUNT_OUTPUT_STATEMENT);
         return Console.readLine();
     }
 
     public String getLottoWinningNumber() {
-        System.out.println("당첨 번호를 입력해 주세요.");
+        System.out.println(LOTTO_WINNING_NUMBER_OUTPUT_STATEMENT);
+        return Console.readLine();
+    }
+
+    public String getLottoBonusNumber() {
+        System.out.println(LOTTO_BONUS_NUMBER_OUTPUT_STATEMENT);
         return Console.readLine();
     }
 }
