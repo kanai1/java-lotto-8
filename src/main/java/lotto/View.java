88f201345e120504ab95c80d20e2ac07ddf49879
kanai1 <ksh30918@kakao.com>
2025-10-29 01:04:00 +0900

    feat(View): printWinningDetail 메소드 추가
    - 로또 당첨 내역을 출력하는 메소드

--- src/main/java/lotto/View.java
+++ src/main/java/lotto/View.java
@@ -9,6 +9,13 @@ public class View {
     private static final String LOTTO_WINNING_NUMBER_OUTPUT_STATEMENT = "당첨 번호를 입력해 주세요.";
     private static final String LOTTO_BONUS_NUMBER_OUTPUT_STATEMENT = "보너스 번호를 입력해 주세요.";
     private static final String PURCHASED_LOTTO_NUMBER_OUTPUT_STATEMENT = "개를 구매했습니다.";
+    private static final List<String> WINNING_DETAIL_OUTPUT_STATEMENTS = List.of(
+            "3개 일치 (5,000원) - %d개\n",
+            "4개 일치 (50,000원) - %d개\n",
+            "5개 일치 (1,500,000원) - %d개\n",
+            "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n",
+            "6개 일치 (2,000,000,000원) - %d개\n"
+    );
 
     public String getLottoPurchaseAmount() {
         System.out.println(LOTTO_PURCHASE_AMOUNT_OUTPUT_STATEMENT);
@@ -29,6 +36,12 @@ public class View {
         System.out.println(tickets.size() + PURCHASED_LOTTO_NUMBER_OUTPUT_STATEMENT);
         for (Lotto lotto: tickets) {
             System.out.println("["+String.join(", ", String.join(", ", lotto.getNumbersString()))+"]");
+        }
+    }
+
+    public void printWinningDetail(List<Integer> rank) {
+        for (int i = 0; i < 5; i++) {
+            System.out.printf(WINNING_DETAIL_OUTPUT_STATEMENTS.get(i), rank.get(i));
         }
     }
 }
