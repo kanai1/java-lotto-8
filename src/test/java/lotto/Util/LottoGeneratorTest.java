1edd8f0ca58194eceadcae3eb8442aa4fbde7159
kanai1 <ksh30918@kakao.com>
2025-10-29 14:09:33 +0900

    test(Util.generator): 로또 난수 생성 테스트
    - 로또 난수 생성을 테스트
    - 중복되는 숫자가 없어야 하고 각 숫자는 1~45 사이에 있어야함

---
+++ src/test/java/lotto/Util/LottoGeneratorTest.java
@@ -0,0 +1,22 @@
+package lotto.Util;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.HashSet;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+
+public class LottoGeneratorTest {
+
+    @RepeatedTest(100)
+    @DisplayName("로또 난수 생성 테스트")
+    void generatorTest() {
+        LottoGenerator lottoGenerator = new RandomLottoGenerator();
+        List<Integer> lotto = lottoGenerator.generate();
+        assertThat(new HashSet<Integer>(lotto).size()).isEqualTo(6); // 중복확인
+        for (Integer num: lotto) {
+            assertThat(num).isBetween(1, 45);
+        }
+    }
+}
