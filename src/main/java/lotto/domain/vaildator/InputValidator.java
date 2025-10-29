f50b00ea94254a2ca5ef124b0d765c7c8aebaa89
kanai1 <ksh30918@kakao.com>
2025-10-29 15:28:07 +0900

    refactor(InputValidator): 변수명 변경
    - 매개변수를 input으로 변경
    - 금액을 저장하는 내부변수를 amount로 변경

--- src/main/java/lotto/domain/vaildator/InputValidator.java
+++ src/main/java/lotto/domain/vaildator/InputValidator.java
@@ -1,16 +1,20 @@
 package lotto.domain.vaildator;
 
 public class InputValidator {
-    public void purchaseAmountValidator(String amount) {
-        int money;
+    public void purchaseAmountValidator(String input) {
+        int amount;
         try {
-            money = Integer.parseInt(amount);
+            amount = Integer.parseInt(input);
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("구매금액은 숫자로 이루어져야 합니다.");
         }
 
-        if (money % 1000 != 0) {
+        if (amount % 1000 != 0) {
             throw new IllegalArgumentException("구매금액은 1000원 단위로 나누어 떨어져야 합니다.");
         }
+    }
+    
+    public void lottoWinningNumberValidator(String input) {
+
     }
 }
