fe28134b88577d7e9c3473abde6afc7e155a5684
kanai1 <ksh30918@kakao.com>
2025-10-29 16:35:49 +0900

    test(Lotto): 로또 검증에 관한 테스트 추가
    - 입력값이 숫자인지 여부
    - 입력값이 6개인지 여부
    - 입력값에 중복은 없는지 여부

--- src/test/java/lotto/domain/lotto/LottoTest.java
+++ src/test/java/lotto/domain/lotto/LottoTest.java
@@ -1,16 +1,34 @@
 package lotto.domain.lotto;
 
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class LottoTest {
+
+    static Stream<Arguments> argumentsForOfTest() {
+        return Stream.of(
+                Arguments.of("1,2,3,4,5", "로또 번호는 6개여야 합니다"),
+                Arguments.of("1,2,3,4,5,6,7", "로또 번호는 6개여야 합니다"),
+                Arguments.of("", "로또 번호"),
+                Arguments.of("a,b,c,d", "로또 번호"),
+                Arguments.of("a,b,c,d,e,f", "로또 번호는 1부터 45 사이의 숫자여야 합니다"),
+                Arguments.of("0,1,2,3,4,5", "로또 번호는 1부터 45 사이의 숫자여야 합니다"),
+                Arguments.of("1,2,3,4,5,46", "로또 번호는 1부터 45 사이의 숫자여야 합니다"),
+                Arguments.of("1,2,3,4,5,5", "로또 번호는 중복되어선 안됩니다")
+        );
+    }
+
     @Test
-    @Disabled
     void 로또_번호의_개수가_6개가_넘어가면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 6, 7)))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -18,11 +36,22 @@ class LottoTest {
 
     @DisplayName("로또 번호에 중복된 숫자가 있으면 예외가 발생한다.")
     @Test
-    @Disabled
     void 로또_번호에_중복된_숫자가_있으면_예외가_발생한다() {
         assertThatThrownBy(() -> new Lotto(List.of(1, 2, 3, 4, 5, 5)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    // TODO: 추가 기능 구현에 따른 테스트 코드 작성
+    @ParameterizedTest(name = "{displayName}({0})")
+    @DisplayName("로또 검증 테스트")
+    @MethodSource("argumentsForOfTest")
+    void ofTest(String input, String errorMessage) {
+        assertThatThrownBy(() -> Lotto.of(input))
+                .isInstanceOf(IllegalArgumentException.class).hasMessageContaining(errorMessage);
+    }
+
+    @Test
+    @DisplayName("로또 검증 테스트(1,2,3,4,5,6)")
+    void ofSuccessTest() {
+        assertThatCode(() -> Lotto.of("1,2,3,4,5,6")).doesNotThrowAnyException();
+    }
 }
