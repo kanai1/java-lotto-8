7dad8dc3d3309a618584cab4fd4f0405bbd2794e
kanai1 <ksh30918@kakao.com>
2025-10-30 20:35:17 +0900

    refactor(OutputView): 상수명 변경 및 기타 수정
    - printf로 출력하는 상수에 대해서 상수명을 _FORMAT으로 변경
    - 로또 구매 개수 출력을 println에서 printf로 변경

--- src/main/java/lotto/View/OutputView.java
+++ src/main/java/lotto/View/OutputView.java
@@ -5,19 +5,19 @@ import lotto.domain.lotto.Lotto;
 
 public class OutputView {
 
-    private static final String PURCHASED_LOTTO_NUMBER_OUTPUT_STATEMENT = "개를 구매했습니다.";
-    private static final List<String> WINNING_DETAIL_OUTPUT_STATEMENTS = List.of(
+    private static final String PURCHASED_LOTTO_NUMBER_OUTPUT_FORMAT = "%d개를 구매했습니다.\n";
+    private static final List<String> WINNING_DETAIL_OUTPUT_FORMAT = List.of(
             "3개 일치 (5,000원) - %d개\n",
             "4개 일치 (50,000원) - %d개\n",
             "5개 일치 (1,500,000원) - %d개\n",
             "5개 일치, 보너스 볼 일치 (30,000,000원) - %d개\n",
             "6개 일치 (2,000,000,000원) - %d개\n"
     );
-    private static final String WINNING_RATE_OUTPUT_STATEMENT = "총 수익률은 %.1f%%입니다.\n";
-    private static final String ERROR_OUTPUT_STATEMENT = "[ERROR] %s\n";
+    private static final String WINNING_RATE_OUTPUT_FORMAT = "총 수익률은 %.1f%%입니다.\n";
+    private static final String ERROR_OUTPUT_FORMAT = "[ERROR] %s\n";
 
     public void printPurchasedLottoNumber(List<Lotto> tickets) {
-        System.out.println(tickets.size() + PURCHASED_LOTTO_NUMBER_OUTPUT_STATEMENT);
+        System.out.printf(PURCHASED_LOTTO_NUMBER_OUTPUT_FORMAT, tickets.size());
         for (Lotto lotto: tickets) {
             System.out.println("["+String.join(", ", String.join(", ", lotto.getNumbersString()))+"]");
         }
@@ -25,15 +25,15 @@ public class OutputView {
 
     public void printWinningDetail(List<Integer> rank) {
         for (int i = 0; i < 5; i++) {
-            System.out.printf(WINNING_DETAIL_OUTPUT_STATEMENTS.get(i), rank.get(i));
+            System.out.printf(WINNING_DETAIL_OUTPUT_FORMAT.get(i), rank.get(i));
         }
     }
 
     public void printWinningRate(float winningRate) {
-        System.out.printf(WINNING_RATE_OUTPUT_STATEMENT, winningRate);
+        System.out.printf(WINNING_RATE_OUTPUT_FORMAT, winningRate);
     }
 
     public void printError(String errorMessage) {
-        System.out.printf(ERROR_OUTPUT_STATEMENT, errorMessage);
+        System.out.printf(ERROR_OUTPUT_FORMAT, errorMessage);
     }
 }
