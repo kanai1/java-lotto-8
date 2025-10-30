837dc65c9aabc940d647279434bd2968e6fb575a
kanai1 <ksh30918@kakao.com>
2025-10-30 22:02:38 +0900

    feat(InputValidator): 음수 입력에 검증 추가

--- src/main/java/lotto/domain/vaildator/InputValidator.java
+++ src/main/java/lotto/domain/vaildator/InputValidator.java
@@ -8,7 +8,9 @@ public class InputValidator {
         } catch (NumberFormatException e) {
             throw new IllegalArgumentException("구매금액은 숫자로 이루어져야 합니다.");
         }
-
+        if (amount < 1000) {
+            throw new IllegalArgumentException("구매금액은 1000원 이상이여야 합니다.");
+        }
         if (amount % 1000 != 0) {
             throw new IllegalArgumentException("구매금액은 1000원 단위로 나누어 떨어져야 합니다.");
         }
