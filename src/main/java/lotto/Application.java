cdd3ad58526ca7398214584a6837f7937ec58292
kanai1 <ksh30918@kakao.com>
2025-10-30 15:17:15 +0900

    feat(Application): 메인로직 호출
    - LottoMachine 호출 코드 추가

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,10 +1,24 @@
 package lotto;
 
+import lotto.Util.LottoGenerator;
+import lotto.Util.RandomLottoGenerator;
+import lotto.View.InputView;
 import lotto.View.OutputView;
+import lotto.domain.vaildator.InputValidator;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
-        OutputView view = new OutputView();
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        InputValidator inputValidator = new InputValidator();
+        LottoGenerator lottoGenerator = new RandomLottoGenerator();
+
+        LottoMachine lottoMachine = new LottoMachine(
+                inputView,
+                outputView,
+                inputValidator,
+                lottoGenerator);
+
+        lottoMachine.run();
     }
 }
