ee78084f4cbe5bdbf033a15b1e710c9eb1674ae5
kanai1 <ksh30918@kakao.com>
2025-10-30 20:38:56 +0900

    refactor(LottoGenerator): 일부 숫자에 대해서 상수로 관리
    - 랜덤함수 호출을 위한 매개변수를 상수로 관리

--- src/main/java/lotto/Util/RandomLottoGenerator.java
+++ src/main/java/lotto/Util/RandomLottoGenerator.java
@@ -4,8 +4,13 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class RandomLottoGenerator implements LottoGenerator{
+
+    private static final int MINIMUM_NUMBER = 1;
+    private static final int MAXIMUM_NUMBER = 45;
+    private static final int COUNT_OF_LOTTO = 6;
+
     @Override
     public List<Integer> generate() {
-        return Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        return Randoms.pickUniqueNumbersInRange(MINIMUM_NUMBER, MAXIMUM_NUMBER, COUNT_OF_LOTTO);
     }
 }
